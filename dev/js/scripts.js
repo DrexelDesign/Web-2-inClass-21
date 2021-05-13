bc3c1386c9785a5c97ba3f52dd9e406999ed4c4f
Josh <josh@gdovindesigns.com>
2021-05-13 13:14:05 -0400

    demo done

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,17 @@
 
+import $ from "jquery";
+
+let $nav = $('nav[data-nav]');
+let isVisible = false;
+
+$(".nav-btns").on("click", function(){
+    // console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+})
